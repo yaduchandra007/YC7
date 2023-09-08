a6fb6b6f7ff459f00e36791fba87a98842cd9179
Yadu Chandra A U <yadu.au@intimetec.com>
2023-09-08 12:16:40 +0530

    added css and js file

---
+++ assets/js/main.js
@@ -0,0 +1,105 @@
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navItem = document.querySelectorAll(".nav__item"),
+  header = document.getElementById("header");
+
+// open and close menu
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__menu--open");
+  changeIcon();
+});
+
+// close the menu when the user clicks the nav links
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (navMenu.classList.contains("nav__menu--open")) {
+      navMenu.classList.remove("nav__menu--open");
+    }
+    changeIcon();
+  });
+});
+
+// Change nav toggle icon
+function changeIcon() {
+  if (navMenu.classList.contains("nav__menu--open")) {
+    navToggle.classList.replace("ri-menu-3-line", "ri-close-line");
+  } else {
+    navToggle.classList.replace("ri-close-line", "ri-menu-3-line");
+  }
+}
+
+// header scroll animation
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    header.classList.add("header--scroll");
+  } else {
+    header.classList.remove("header--scroll");
+  }
+});
+
+// ScrollReveal animations
+const sr = ScrollReveal({
+  duration: 2000,
+  distance: "100px",
+  delay: 400,
+  reset: false,
+});
+
+sr.reveal(".hero__content, .about__content");
+sr.reveal(".hero__img", { origin: "top" });
+
+sr.reveal(
+  ".hero__info-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .service__card, .project__content, .testimonial__wrapper, .footer__content",
+  {
+    delay: 500,
+    interval: 100,
+  }
+);
+
+sr.reveal(".qualification__footer-text, .contact__content", {
+  origin: "left",
+});
+
+sr.reveal(".qualification__footer .btn, .contact__btn", { origin: "right" });
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Software Developer", "MERN Stack Developer", "Frontend Developer", "Backend Developer"];
+const typingDelay = 150;
+const erasingDelay = 50;
+const newTextDelay = 50; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan?.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
